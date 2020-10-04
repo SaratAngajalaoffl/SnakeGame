4a5e7c37f178a7079038ac8555887d425d695d67
Sarat Angajala <chandrasarat74@gmail.com>
2020-10-04 17:31:33 +0530

    Initial Utils done

--- utils/game.py
+++ utils/game.py
@@ -1,12 +1,29 @@
+import random
 from .snake import Snake
 from .fruit import Fruit
 
 
 class Game:
-    WIN_WIDTH = WIN_HEIGHT = 600
+    WIN_WIDTH = WIN_HEIGHT = 50
 
     def __init__(self):
         self.game_canvas = [[0, 0, 0]
                             for _ in range(WIN_WIDTH) for _ in range(WIN_HEIGHT)]
-        self.snake = Snake(self.WIN_WIDTH/2, self.WIN_HEIGHT/2)
-        
+        self.snake = Snake(self.WIN_WIDTH/2, self.WIN_HEIGHT/2, self)
+        self.fruit = Fruit(random.randint(1, WIN_WIDTH),
+                           random.randint(1, WIN_HEIGHT))
+
+    def update_canvas(self):
+
+        for pos in self.snake.pos:
+            self.game_canvas[pos[0]][pos[1]] = [255, 255, 255]
+
+        self.game_canvas[self.fruit.posx][self.fruit.posy] = [255, 0, 0]
+
+    def endgame(self):
+        return "QUIT"
+
+    def new_fruit(self):
+        del self.fruit
+        self.fruit = Fruit(random.randint(1, WIN_WIDTH),
+                           random.randint(1, WIN_HEIGHT))
--- utils/snake.py
+++ utils/snake.py
@@ -3,18 +3,45 @@ class Snake:
     def __init__(self, game, posx, posy):
         self.game = game
         self.pos = [[posx, posy]]
-        self.velx = 2
-        self.vely = 2
+        self.vel = 1
+        self.direction = None
 
     def update_length(self):
-        pass
+        if self.direction is "HORIZONTAL":
+            self.pos.append([self.pos[-1][0]-self.velx, self.pos[-1][1]])
+        if self.direction is "VERTICAL":
+            self.pos.append([self.pos[-1][0], self.pos[-1][1]-self.vely])
 
-    def check_collision(self)
-        pass
-    
+    def check_collision(self):
+        # Check collision with edges
+        if self.pos[0][0] == 0 or self.pos[0][0] == self.game.WIN_WIDTH or self.pos[0][1] == 0 or self.pos[0][1] == self.game.WIN_HEIGHT:
+            self.game.endgame()
+        # Check collision with self
+        if self.pos[0] in self.pos[1:]:
+            self.game.endgame()
+        # Check collision with fruit
+        if self.pos[0] == [game.fruit.posx, game.fruit.posy]:
+            self.update_length()
+            self.game.new_fruit()
+
+    def update_direction(self, direction):
+        if direction == "RIGHT":
+            self.direction = "HORIZONAL"
+            self.vel = abs(self.vel)
+        if direction == "LEFT":
+            self.direction = "HORIZONAL"
+            self.vel = -abs(self.vel)
+        if direction == "UP":
+            self.direction = "VERTICAL"
+            self.vel = -abs(self.vel)
+        if direction == "DOWN":
+            self.direction = "VERTICAL"
+            self.vel = abs(self.vel)
+
     def move(self):
-        pass
-    
-    def update_velocity(self):
-        pass
-    
+        if self.direction == "HORIZONTAL":
+            self.pos[0] = [self.pos[0][0] + vel, self.pos[0][1]]
+        if self.direction == "VERTICAL":
+            self.pos[0] = [self.pos[0][0], self.pos[0][1] + vel]
+        for _, i in enumerate(self.pos[1:-1]):
+            self.pos[i] = self.pos[i+1]
